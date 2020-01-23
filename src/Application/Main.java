ff8f51935de3a1f1778c36cd5903e710de0d6f2c
Marcos Vinicius <sleyer157@gmail.com>
2020-01-23 19:40:21 -0300

    Projeto criado

---
+++ src/Application/Main.java
@@ -0,0 +1,13 @@
+package Application;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 Scanner s = new Scanner (System.in);
+
+.,/~~ppppppooiiuuhhjhhjhjuuhabsmarcos viniciius santos rocha karalho viado do whileR seu arrombado     
+	}
+
+}
